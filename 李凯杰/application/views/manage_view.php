cd48631fca051e3c969bbd1a2d627c1b512fdd53
Ji Zihao <zhao11fs@gmail.com>
2014-10-08 18:46:10 +0800

    add task.md

--- 李凯杰/application/views/manage_view.php
+++ 李凯杰/application/views/manage_view.php
@@ -45,7 +45,7 @@
 	#body{
 		margin: 0 15px 0 15px;
 	}
-	
+
 	p.footer{
 		text-align: right;
 		font-size: 11px;
@@ -54,7 +54,7 @@
 		padding: 0 10px 0 10px;
 		margin: 20px 0 0 0;
 	}
-	
+
 	#container{
 		margin: 10px;
 		border: 1px solid #D0D0D0;
@@ -66,56 +66,56 @@
 
 <div id="container">
 	<h1>说说:</h1>
-	<?=form_open('blog/sayInsert');?>
-	<p><?=form_textarea('body',"");?></p>
+<?=form_open('blog/sayInsert');?>
+	<p><?=form_textarea('body', "");?></p>
 	<p><input type="submit" value="发布" /></p>
 	</form>
 	<div id="body">
-		<?php if($says->num_rows() > 0): ?>
-			<?php foreach($says->result() as $row): ?>
+<?php if ($says->num_rows() > 0):?>
+			<?php foreach ($says->result() as $row):?>
 				<h4><?=$row->body?></h4>
-				<p><?=date("Y-m-d H:i:s",$row->time)?> <?=anchor('blog/saysDel/'.$row->id, "删除");?> </p>
+				<p><?=date("Y-m-d H:i:s", $row->time)?> <?=anchor('blog/saysDel/' . $row->id, "删除");?></p>
 				<hr>
-			<?php endforeach; ?>
-		<?php endif; ?>
-	</div>
+<?php endforeach;?>
+		<?php endif;?>
+</div>
 </div>
 
 <div id="container">
 	<h1>日志:</h1>
-	<?=anchor('blog/textEdit/0', "新日志...");?>
-	<?php if($entries->num_rows() > 0): ?>
-		<?php foreach($entries->result() as $row): ?>
+<?=anchor('blog/textEdit/0', "新日志...");?>
+	<?php if ($entries->num_rows() > 0):?>
+		<?php foreach ($entries->result() as $row):?>
 			<div id="container">
 				<h1><?=$row->title?></h1>
 				<p><?=$row->body?></p>
-				<p><?=date("Y-m-d H:i:s",$row->time)?> <?=anchor('blog/textEdit/'.$row->id, "编辑");?> <?=anchor('blog/textDel/'.$row->id, "删除");?> </p>
+				<p><?=date("Y-m-d H:i:s", $row->time)?> <?=anchor('blog/textEdit/' . $row->id, "编辑");?> <?=anchor('blog/textDel/' . $row->id, "删除");?></p>
 				<hr>
 				<div id="body">
-					<?php foreach($comments->result() as $comment): ?>
-						<?php if($comment->entry_id == $row->id): ?>
+<?php foreach ($comments->result() as $comment):?>
+						<?php if ($comment->entry_id == $row->id):?>
 							<h4><?=$comment->author?> said:</h4>
 							<p><?=$comment->body?></p>
-							<p><?=date("Y-m-d H:i:s", $comment->time)?> <?=anchor('blog/commentDel/'.$comment->id."/".$row->id, "删除");?></p>
-						<?php endif; ?>
-					<?php endforeach; ?>
-				</div>
+							<p><?=date("Y-m-d H:i:s", $comment->time)?> <?=anchor('blog/commentDel/' . $comment->id . "/" . $row->id, "删除");?></p>
+<?php endif;?>
+					<?php endforeach;?>
+</div>
 			</div>
-		<?php endforeach; ?>
-	<?php endif; ?>
+<?php endforeach;?>
+	<?php endif;?>
 </div>
 <div id="container">
 	<h1>留言板:</h1>
-	<?php if($messages->num_rows() > 0): ?>
-		<?php foreach($messages->result() as $row): ?>
+<?php if ($messages->num_rows() > 0):?>
+		<?php foreach ($messages->result() as $row):?>
 			<div id="body">
 				<p><?=$row->message?></p>
 				<h4>by: <?=$row->author?></h4>
-				<p><?=date("Y-m-d H:i:s", $row->time)?> <?=anchor('blog/messageDel/'.$row->id, "删除");?> </p>
+				<p><?=date("Y-m-d H:i:s", $row->time)?> <?=anchor('blog/messageDel/' . $row->id, "删除");?></p>
 				<hr>
 			</div>
-		<?php endforeach; ?>
-	<?php endif; ?>
+<?php endforeach;?>
+	<?php endif;?>
 </div>
 <p><?=anchor('blog', "返回");?></p>
 
